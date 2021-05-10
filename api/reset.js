fb9d35f54b968b66dee5885ebe91bd6ec5538804
raviroygit <ravi93448@yahoo.com>
2021-05-10 17:17:08 +0530

    email

--- api/reset.js
+++ api/reset.js
@@ -41,7 +41,7 @@ router.post("/", async (req, res) => {
 
     const mailOptions = {
       to: user.email,
-      from: "singh.inder5880@gmail.com",
+      from: "social@codelength.net",
       subject: "Hi there! Password reset request",
       html: `<p>Hey ${user.name
         .split(" ")[0]
